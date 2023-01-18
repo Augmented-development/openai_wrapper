489d97311f5e3d0d806944c88db19a6e26b7c02f
Petr Lavrov <calmquant@gmail.com>
2023-01-19 01:53:36 +0300

    first commit, move from chatgpt bot

---
+++ openai_wrapper/__init__.py
@@ -0,0 +1,1 @@
+from .wrapper import OpenaiWrapper, get_openai_wrapper, DEFAULT_QUERY_CONFIG
---
+++ openai_wrapper/wrapper.py
@@ -0,0 +1,116 @@
+import getpass
+import json
+import os
+import os.path
+from dataclasses import dataclass
+from typing import Union, List
+
+import openai
+
+
+@dataclass
+class QueryConfig:
+    model: str = "text-davinci-003"
+    max_tokens: int = 512
+    temperature: float = 0.9
+    top_p: float = 1.0
+    n: int = 1
+    stream: bool = False
+    stop: Union[str, List[str]] = None
+    user: str = getpass.getuser()
+
+    def update(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+DEFAULT_QUERY_CONFIG = QueryConfig()
+
+
+class OpenaiWrapper:
+    api = openai
+
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    def _query(self, prompt, config):
+        # make sure api_key is correct
+        self.api.api_key = self.api_key
+
+        response = self.api.Completion.create(
+            model=config.model,
+            prompt=prompt,
+            max_tokens=config.max_tokens,
+            temperature=config.temperature,
+            top_p=config.top_p,
+            n=config.n,
+            stream=config.stream,
+            stop=config.stop,
+            user=config.user,
+        )
+
+        return response
+
+    def query(self, prompt: str, config: QueryConfig = DEFAULT_QUERY_CONFIG, **kwargs) -> str:
+        config.update(**kwargs)
+        response = self._query(prompt, config)
+        return response.choices[0].text
+
+    def query_cheap(self, prompt: str, **kwargs) -> str:
+        if "model" in kwargs:
+            raise ValueError("For cheap query model cannot be overridden")
+        return self.query(prompt, model="text-curie-001", **kwargs)
+
+
+registry = {}
+
+possible_filenames = [
+    '.openai_api_key',
+    'secrets.json',
+    'secrets.txt',
+]
+
+possible_key_locations = [
+    os.path.expanduser('~'),
+    os.path.abspath(os.path.dirname(__file__)),
+    os.path.abspath('')
+]
+
+
+def get_openai_wrapper(api_key=None) -> OpenaiWrapper:
+    if api_key is None:
+        api_key = os.environ.get("OPENAI_API_KEY")
+    # if the object already exists, return it
+    if api_key in registry:
+        return registry[api_key]
+    if api_key is None:
+        for location in possible_key_locations:
+            if api_key is not None:
+                break
+            for filename in possible_filenames:
+                full_path = os.path.join(location, filename)
+                if os.path.exists(full_path):
+                    with open(full_path, 'r') as f:
+                        data = f.read()
+                        if filename.endswith('.json'):
+                            data = json.loads(data)
+                            if 'openai_api_key' in data:
+                                api_key = data['openai_api_key']
+                                break
+                        elif filename.endswith('.txt'):
+                            # noinspection PyTypeChecker
+                            d = dict([line.split(':', 1) for line in data.splitlines()])
+                            if 'openai_api_key' in d:
+                                api_key = d['openai_api_key']
+                                break
+                        elif filename.startswith('.'):
+                            # check it's only one line:
+                            if len(data.strip().splitlines()) == 1:
+                                api_key = data.strip()
+                                break
+        if api_key is None:
+            message = "Please enter your OpenAI API key. You can find it at https://beta.openai.com/account/api-keys"
+            api_key = getpass.getpass(message)
+
+    openai_wrapper = OpenaiWrapper(api_key)
+    registry[api_key] = openai_wrapper
+    return openai_wrapper
